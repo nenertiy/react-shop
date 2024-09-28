938c2d240b428505e468e0c776ce8e2ab39daede
nenertiy <nenertiy@gmail.com>
2024-09-28 15:42:48 +0300

    swiper error

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { API_URL } from "../../utils/constants";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 
 import "swiper/scss";
 
